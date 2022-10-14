0930a8aa1d7f7fb6ebcda4cbbfb5e11e05d3af5d
Malygina Svetlana <malygina25@yandex.ru>
2022-10-14 18:08:33 +0300

    4

---
+++ 4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа принимает число (N) и показывает все чётные числа от 1 до N.");
+Console.WriteLine("Введите число: ");
+int h = int.Parse(Console.ReadLine());
+int startNumber = 1;
+int i = 0;
+for (i = startNumber; i <= h; i++) {
+    if(i % 2 == 0){
+        Console.WriteLine("Четное число: "+ i);
+    }
+}
