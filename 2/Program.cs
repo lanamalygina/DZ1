9001dfb33e081beb3c2b898203295be352a4e7dd
Malygina Svetlana <malygina25@yandex.ru>
2022-10-14 18:03:09 +0300

    2

---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа принимает три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine("Введите первое число: ");
+int c = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int d = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int f = int.Parse(Console.ReadLine());
+
+int max = c;
+
+if (c > max) max = c;
+if (d > max) max = d;
+if (f > max) max = f;
+
+Console.WriteLine("Максимальное число = "+ max);
