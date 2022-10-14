d609998c5be1adc2345a8a61fb2d548f4824c97e
Malygina Svetlana <malygina25@yandex.ru>
2022-10-14 18:06:50 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Программа принимает число и сообщает, является ли число чётным");
+Console.WriteLine("Введите число: ");
+int e = int.Parse(Console.ReadLine());
+
+int number = e % 2;
+
+if(number > 0)
+{
+    Console.WriteLine("Такое число нечётное");
+}
+else
+{
+    Console.WriteLine("Это число чётное");
+}
