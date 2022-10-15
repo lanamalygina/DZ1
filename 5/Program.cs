2da52b8ce85b70323cb6f4a85a45040b52b44312
Malygina Svetlana <malygina25@yandex.ru>
2022-10-15 09:25:27 +0300

    5

---
+++ 5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Задание №10");
+Console.WriteLine("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+string gg = Convert.ToString(number);
+
+Console.WriteLine("Вторая цифра числа: " + gg[1]);
