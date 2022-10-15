bce0724f57a25beda06cf25c68604cd401b14fb4
Malygina Svetlana <malygina25@yandex.ru>
2022-10-15 09:37:16 +0300

    7

---
+++ 7/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Задание 15 ");
+
+Console.WriteLine("Принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. ");
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+    {
+        Console.WriteLine("этот день выходной");
+    }
+  else if (dayNumber < 1 || dayNumber > 7) 
+    {
+        Console.WriteLine("это не день недели");
+    }
+  else 
+        Console.WriteLine("этот день не выходной");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
